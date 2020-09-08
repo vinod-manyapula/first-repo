199c3bf166cb7174dcde79026ac6d413b4bfd881
vinod-manyapula <vinodcse564316@gmail.com>
2020-09-07 23:33:10 -0400

    rest webservice

---
+++ notes.md
@@ -0,0 +1,15 @@
+# RESTful Web Services
+
+Social Media Application
+
+User -> Posts
+
+- Retrieve all users    - GET /users
+- Create a user 		- POST /users
+- Retrieve one users 	- GET /users/{id} -> /users/1
+- Delete a user 		- Delete /users/{id} -> /users/1
+
+- Retrieve all posts for a User - GET /users/{id}/posts
+- Create a posts for a User 	- POST /users/{id}/posts
+- Retrieve details of a postr 	- GET /users/{id}/posts/{post_id}
+
---
+++ src/main/java/com/vinod/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,21 @@
+package com.vinod.rest.webservices.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message=message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message){
+        this.message=message;
+    }
+    @Override
+    public String toString(){
+        return String.format("HelloWorldBean [message=%s]", message);
+    }
+}

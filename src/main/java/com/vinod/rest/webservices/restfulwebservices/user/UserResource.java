83aff74fbe7c0d8a452c5d0d0b76fa1f75bf5cd3
vinod-manyapula <vinodcse564316@gmail.com>
2020-09-08 00:38:59 -0400

    201 status code

--- src/main/java/com/vinod/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/vinod/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.vinod.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,7 +25,12 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity createUser(@RequestBody User user){
         User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}").
+                buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
